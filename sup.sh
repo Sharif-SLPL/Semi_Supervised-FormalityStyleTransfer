be2a3268acbdb17d8b8a6b80a9b3dc38f3c3a52e
Mona_Ebrahimi <69147278+MonaEbrahimi-SUT@users.noreply.github.com>
2022-08-28 22:16:02 +0430

    Update sup.sh

--- sup.sh
+++ sup.sh
@@ -2,8 +2,8 @@
 #Run Supervised Training Part
 CUDA_VISIBLE_DEVICES=0 python Style_Transfer.py \
     -style 0 \
-    -dataset em \
-    -order em.sup \
+    -dataset digi \
+    -order digi.sup \
     -batch_size 8 \
     -val_batch_size 16 \
     -lr 2e-5
