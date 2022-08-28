b86e25192a0e22cbd126c730e4cbe02de43b3178
Mona_Ebrahimi <69147278+MonaEbrahimi-SUT@users.noreply.github.com>
2022-08-28 15:19:13 +0430

    Add files via upload

---
+++ runsemi.sh
@@ -0,0 +1,30 @@
+
+#Run Supervised Training Part
+CUDA_VISIBLE_DEVICES=0 python T5_Large_filter.py \
+    -style 0 \
+    -dataset em \
+    -order em.sup \
+    -batch_size 8 \
+    -val_batch_size 16 \
+    -lr 2e-5
+
+
+
+#Run Semi-Supervised Training Part
+CUDA_VISIBLE_DEVICES=0 python T5_Large_filter.py \
+    -unsup \
+    -style 0 \
+    -ratio 1.0 \
+    -dataset em \
+    -order em-semi \
+    -pre_step 2000 \
+    -batch_size 8 \
+    -unsup_batch_size 16 \
+    -val_batch_size 16 \
+    -lr 2e-5 \
+    -aug_choice none \
+    -aug_p 0.1 \
+    -filter none \
+    -phi 0.4 \
+    -n_step 1000 \
+    -sigma 0.8
